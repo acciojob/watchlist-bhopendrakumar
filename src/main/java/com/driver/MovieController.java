4dc2b5603efc1547798248b9ae8a2ff593fc148c
[bhopendra] <[bhopendrasahu2899@gmail.com]>
2022-10-31 23:47:56 +0530

    last

--- src/main/java/com/driver/MovieController.java
+++ src/main/java/com/driver/MovieController.java
@@ -13,9 +13,8 @@ import java.util.Map;
 public class MovieController {
     HashMap<String,Movie> allmovie=new HashMap<>();
     HashMap<String,Director> allDirecter=new HashMap<>();
-    HashMap<String,String> forupdate=new HashMap<>();
-    HashMap<String,ArrayList<String>> pairs=new HashMap<>();
-    ArrayList<Movie> allMoviein=new ArrayList<>();
+    HashMap<String,List<String>> pairs=new HashMap<>();
+
 
     // travers all movies
     @GetMapping("/get-all-movies")
@@ -53,72 +52,58 @@ public class MovieController {
 
 
     @PostMapping("/add-movie")
-    public String addMovie(@RequestBody Movie movie){
+    public ResponseEntity<String> addMovie(@RequestBody Movie movie){
         allmovie.put(movie.getName(),movie);
-        return  "success";
+        return new ResponseEntity<>("successfully", HttpStatus.CREATED);
     }
     @PostMapping("/add-director")
-    public String addDirector(@RequestBody Director director){
+    public ResponseEntity<String> addDirector(@RequestBody Director director){
         allDirecter.put(director.getName(),director);
-        return "success";
+        return new ResponseEntity<>("successfully", HttpStatus.CREATED);
     }
 
 
     @PutMapping("/add-movie-director-pair/{movie_name}/{director_name}")
-    public String addMovieDirectorPair(@PathVariable String movie_name,@PathVariable String director_name){
-
-        forupdate.put(movie_name,director_name);
-
-//        if(pairs.containsKey(director_name))
-//            pairs.get(director_name).add(movie_name);
-//        else{
-//            ArrayList<String> temp=new ArrayList<>();
-//            temp.add(movie_name);
-//            pairs.put(director_name,temp);
-//        }
-        return "success";
+    public ResponseEntity<String> addMovieDirectorPair(@PathVariable String movie_name,@PathVariable String director_name){
+   if(allmovie.containsKey(movie_name) && allDirecter.containsKey(director_name)){
+       List<String> cur_movie=new ArrayList<>();
+       if(pairs.containsKey(director_name))
+           cur_movie=pairs.get(director_name);
+           cur_movie.add(movie_name);
+            pairs.put(director_name,cur_movie);
+        }
+        return new ResponseEntity<>("successfully", HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete-director-by-name/{director}")
-    public String deleteDirectorByName(@PathVariable String director){
-
-        for (Map.Entry<String,String> entry : forupdate.entrySet()){
-            if(director.equals(entry.getValue())){
-                String moviedel=entry.getKey();
-                if(allmovie.containsKey(moviedel))
-                allmovie.remove(moviedel);
-                if(allDirecter.containsKey(entry.getValue()))
-                    allDirecter.remove(entry.getValue());
-                forupdate.remove(moviedel);
+    public ResponseEntity<String> deleteDirectorByName(@PathVariable String director){
+        List<String> movies=new ArrayList<>();
+        if(pairs.containsKey(director)){
+            movies=pairs.get(director);
+            for(String delmovie:movies){
+                if(allmovie.containsKey(delmovie)){
+                    allmovie.remove(delmovie);
+                }
             }
+            pairs.remove(director);
         }
-//        if(forupdate.containsKey(director)){
-//            if(allmovie.containsKey(forupdate.get(director)))
-//              allmovie.remove(forupdate.get(director));
-//            forupdate.remove(director);
-//        }
-//        if(allDirecter.containsKey(director)){
-//            allDirecter.remove(director);
-//        }
+        if(allDirecter.containsKey(director))
+            allDirecter.containsKey(director);
 
-          return "sucess";
+        return new ResponseEntity<>("successfully", HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete-all-directors")
-    public String deleteAllDirectors(){
-//      for(List<String> temp: pairs.values()){
-//          for(int i=0;i<temp.size();i++) {
-//              if (allmovie.containsKey(temp.get(i)))
-//                  allmovie.remove(temp.get(i));
-//          }
-//      }
-        for(String name : forupdate.keySet()){
-            if (allmovie.containsKey(name))
-            allmovie.remove(name);
-        }
-        forupdate.clear();
-        allDirecter.clear();
-      return "sucess";
+    public ResponseEntity<String> deleteAllDirectors(){
+      for(List<String> temp: pairs.values()){
+          for(int i=0;i<temp.size();i++) {
+              if (allmovie.containsKey(temp.get(i)))
+                  allmovie.remove(temp.get(i));
+          }
+      }
+      allDirecter.clear();
+      pairs.clear();
+        return new ResponseEntity<>("successfully", HttpStatus.CREATED);
     }
 
 }
